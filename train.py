7b37182e1146d892bd37be9267c51f563f42fe5b
PanXiebit <lijiakun@gmail.com>
2020-04-27 21:41:27 +0800

    tpu is soooooooo slow

--- train.py
+++ train.py
@@ -6,6 +6,7 @@ import torch_xla.utils.utils as xu
 import torch_xla.core.xla_model as xm
 import torch_xla.distributed.xla_multiprocessing as xmp
 import torch_xla.test.test_utils as test_utils
+import torch_xla.distributed.data_parallel as dp
 
 import warnings
 warnings.filterwarnings("ignore")
@@ -33,6 +34,7 @@ from fairseq.criterions import LabelSmoothedLengthGan_CrossEntropyCriterion
 from fairseq.optim import FairseqAdam
 from fairseq.optim.lr_scheduler  import InverseSquareRootSchedule
 
+args.use_gpu = False
 
 translation_self = TranslationSelfDataset.load_dictionary(args)
 valid_dataset = translation_self.load_dataset("valid")
@@ -63,31 +65,25 @@ def create_model(args):
 def _prepare_sample(sample, device):
     if sample is None or len(sample) == 0:
         return None
-
     if args.use_gpu:
         sample = utils.move_to_cuda(sample)
     else:
         sample = utils.move_to_tpu(sample, device)
-
     def apply_half(t):
         if t.dtype is torch.float32:
             return t.half()
         return t
-
     if args.fp16:
         sample = utils.apply_to_sample(apply_half, sample)
-
     return sample
 
-
-
-def train_loop_fn(args, model, criterion, optimizer, device, scheduler=None):
+def train_loop_fn(train_loader, args, model, criterion, optimizer, device, scheduler=None):
     model.train()
     criterion.train()
-    for i, sample in enumerate(valid_dataloader):
-        _prepare_sample(sample, device)
+    for i, sample in enumerate(train_loader):
+        sample = _prepare_sample(sample, device)
+        print(sample["target"].shape, sample["target"].device)
         optimizer.zero_grad()
-
         _, _, logging_output = criterion(model, sample)
         logging = criterion.aggregate_logging_outputs([logging_output])
         loss = logging["loss"]
@@ -99,12 +95,12 @@ def train_loop_fn(args, model, criterion, optimizer, device, scheduler=None):
     print('End training: {}'.format(device))
 
 
-def eval_loop_fn(args, model, criterion, device):
+def eval_loop_fn(valid_loader, args, model, criterion, device):
     model.eval()
     criterion.eval()
     valid_logging = []
-    for i, sample in enumerate(valid_dataloader):
-        _prepare_sample(sample, device)
+    for i, sample in enumerate(valid_loader):
+        sample = _prepare_sample(sample, device)
         _, _, logging_output = criterion(model, sample)
         valid_logging.append(logging_output)
     val_log = criterion.aggregate_logging_outputs(valid_logging)
@@ -113,11 +109,16 @@ def eval_loop_fn(args, model, criterion, device):
 
 import multiprocessing
 _LOAD_LOCK = multiprocessing.Lock()
+print(_LOAD_LOCK)
 
-def _mp_fn(args):
+def _mp_fn(rank, args):
+    print("rank", rank)
     device = xm.xla_device()
-    with _LOAD_LOCK:
-        _MODEL.to(device)
+    # devices = (
+    #   xm.get_xla_supported_devices(
+    #       max_devices=args.num_cores) if args.num_cores != 0 else [])
+    # with _LOAD_LOCK:
+    #     _MODEL.to(device)
     xm.master_print('done loading model')
 
     criterion = LabelSmoothedLengthGan_CrossEntropyCriterion(args, translation_self.tgt_dict)
@@ -127,8 +128,14 @@ def _mp_fn(args):
     lr_scheduler = InverseSquareRootSchedule(args, optimizer)
 
     for epoch in range(args.num_epochs):
-        train_loop_fn(args, _MODEL, criterion, optimizer, device)
-        valid_log = eval_loop_fn(args, _MODEL, criterion, device)
+        # train_loop_fn(args, _MODEL, criterion, optimizer, device)
+        # valid_log = eval_loop_fn(args, _MODEL, criterion, device)
+        para_loader = pl.ParallelLoader(valid_dataloader, [device])
+        train_loop_fn(para_loader.per_device_loader(device), args, _MODEL, criterion, optimizer, device)
+        para_loader = pl.ParallelLoader(valid_dataloader, [device])
+        valid_log = eval_loop_fn(para_loader.per_device_loader(device), args, _MODEL, criterion, device)
+        xm.master_print('Finished training epoch {}'.format(epoch))
+
         xm.master_print("Epoch {}, loss {:.4f}, nll_loss {:.4f}, length_loss {:.4f}, dis_loss {:.4f}"
                         .format(epoch, valid_log["loss"], valid_log["nll_loss"],
                                 valid_log["length_loss"], valid_log["dis_loss"]))
@@ -139,9 +146,7 @@ def _mp_fn(args):
 if __name__  == "__main__":
     create_model(args)
     xmp.spawn(_mp_fn, args=(args,), nprocs=args.num_cores, start_method='fork')
-# for i, sample in enumerate(valid_dataloader):
-#     # if i > 3:
-#     #     break
-#     loss, sample_size, logging_output = criterion(model, sample)
-#     logging= criterion.aggregate_logging_outputs([logging_output])
-#     print(logging["loss"], logging["nll_loss"], logging["length_loss"], logging["dis_loss"])
+
+
+
+
